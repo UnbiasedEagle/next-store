3b2ade85d362e71201f846d2a3c91eb1fb8796df
Saurabh Singh <imsaurabhsingh007@gmail.com>
2025-12-07 16:43:36 +0530

    feat: Implement NextAuth.js with Drizzle ORM and integrate shadcn/ui with a new theming system.

---
+++ server/auth.ts
@@ -0,0 +1,13 @@
+import NextAuth from 'next-auth';
+import { DrizzleAdapter } from '@auth/drizzle-adapter';
+import { db } from './db';
+import Google from 'next-auth/providers/google';
+import GitHub from 'next-auth/providers/github';
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: DrizzleAdapter(db),
+  providers: [Google, GitHub],
+  session: {
+    strategy: 'jwt',
+  },
+});
